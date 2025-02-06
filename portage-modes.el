61d8cb63d369a8e6bad24be24e2186015fb3f5be
OpenSauce04 <opensauce04@gmail.com>
2025-02-06 16:56:08 +0000

    `portage-modes-keywords-expression` --> `portage-modes-portage-keywords-expression`

--- portage-modes.el
+++ portage-modes.el
@@ -45,7 +45,7 @@
           (1+ not-newline)))
       (1 'font-lock-function-call-face))))
 
-(defvar portage-modes-keywords-expression (rx(or
+(defvar portage-modes-portage-keywords-expression (rx(or
                                               "*" "**" "alpha" "amd64" "arm" "arm64" "hppa" "loong" "m68k" "mips" "ppc" "ppc64" "riscv" "s390" "sparc" "x86"
                                               "amd64-linux" "arm-linux" "arm64-linux" "ppc64-linux" "riscv-linux" "x86-linux" "arm64-macos" "ppc-macos" "x86-macos" "x64-macos" "x64-solaris")))
 (defvar portage-modes-keywords-mode-font-lock-keywords
@@ -54,14 +54,14 @@
         (group-n 1
           " "
           (zero-or-one "~")
-          (or (regex portage-modes-keywords-expression))
+          (or (regex portage-modes-portage-keywords-expression))
           symbol-end))
       (1 'portage-modes-positive-face))
     ( ,(rx
         (group-n 1
           " -"
           (zero-or-one "~")
-          (or (regex portage-modes-keywords-expression))
+          (or (regex portage-modes-portage-keywords-expression))
           symbol-end))
       (1 'portage-modes-negative-face))))
 
