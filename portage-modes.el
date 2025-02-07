10ac263d717ec771e79fdfc1309ea822ec4ba501
OpenSauce04 <opensauce04@gmail.com>
2025-02-07 10:57:29 +0000

    Bumped version number to 0.5

--- portage-modes.el
+++ portage-modes.el
@@ -4,7 +4,7 @@
 
 ;; Author: OpenSauce <opensauce04@gmail.com>
 ;; URL: https://github.com/OpenSauce04/portage-modes
-;; Version: 0.4
+;; Version: 0.5
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 
 ;;; Commentary:
